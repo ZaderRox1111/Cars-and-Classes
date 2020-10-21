2ff9960e54f7ce946b1cab03f7f04db4132fbe98
Zachariah Derrick <72170300+ZaderRox1111@users.noreply.github.com>
2020-10-21 11:21:14 -0700

    Add files via upload

--- cars.py
+++ cars.py
@@ -1,67 +1,52 @@
 import turtle
 
 def main():
-    harry = turtle.Turtle()
+    car1 = Car('ewad', 10, 'green')
+    
 
-    harry_dir = 0
     done = False
     while done == False:
-        inp = input("wad: ")
+        inp = input()
+        car1.move(5, inp)
+        if car1.move(5, inp) == False:
+            done = True
         
-        for move in inp:
-            if move == 'e':
-                done = True
-
-            if move == 'w':
-                harry.fd(5)
-
-            if move == 'a':
-                harry_dir += 5
-                if harry_dir == 360:
-                    harry_dir = 0
-                harry.seth(harry_dir)
-
-            if move == 'd':
-                harry_dir -= 5
-                if harry_dir == 0:
-                    harry_dir = 360
-                harry.seth(harry_dir)
+    turtle.exitonclick()
 
 class Car:
     
-    def __init__(self, inputs, turn_amount):
+    def __init__(self, inputs, turn_amount, color):
         self.inputs = inputs
         self.turn_amount = turn_amount
+        self.color = color
+        self.turt = turtle.Turtle()
+        self.direction = 0
+        self.turt.color(self.color)
 
-    def move(self, speed):
-        turt = turtle.Turtle()
-        
-        direction = 0
-        done = False
-        while done == False:
-            inp = input()
+    def move(self, speed, inp):
+#        done = False
+#        while done == False:
+#            inp = input()
 
-            for move in inp:
+        for move in inp:
 
-                if move == self.inputs[0]:
-                    done == True
+            if move == self.inputs[0]:
+                return False
 
-                if move == self.inputs[1]:
-                    turt.fd(speed)
+            if move == self.inputs[1]:
+                self.turt.fd(speed)
 
-                if move == self.inputs[2]:
-                    direction += self.turn_amount
-                    if direction >= 360:
-                        direction = 0
-                    turt.seth(direction)
+            if move == self.inputs[2]:
+                self.direction += self.turn_amount
+                if self.direction >= 360:
+                    self.direction = 0
+                self.turt.seth(self.direction)
 
-                if move == self.inputs[3]:
-                    direction -= self.turn_amount
-                    if direction <= 0:
-                        direction = 360
-                    turt.seth(direction)
+            if move == self.inputs[3]:
+                self.direction -= self.turn_amount
+                if self.direction <= 0:
+                    self.direction = 360
+                self.turt.seth(self.direction)
 
 
-car1 = Car("ewad", 10)
-car1.move(5)
-turtle.exitonclick()
+main()
